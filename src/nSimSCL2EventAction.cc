98a08d0a970956b47f687815b972d4ae0c02c212
WOOYOUNG JANG <wyjang.physics@gmail.com>
2021-01-01 22:43:25 -0600

    a unused parameter is removed

--- src/nSimSCL2EventAction.cc
+++ src/nSimSCL2EventAction.cc
@@ -26,18 +26,7 @@ void nSimSCL2EventAction::BeginOfEventAction(const G4Event*)
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
-void nSimSCL2EventAction::EndOfEventAction(const G4Event* event)
-{
-  /*
-  for( unsigned long i = 0; i < event->GetTrajectoryContainer()->size(); i++)
-  {
-    G4cout << "TrackID: "    << event->GetTrajectoryContainer()->GetVector()->at(i)->GetTrackID() << G4endl;
-    G4cout << "Name: "       << event->GetTrajectoryContainer()->GetVector()->at(i)->GetParticleName() << G4endl;
-    G4cout << "InitMom:"     << event->GetTrajectoryContainer()->GetVector()->at(i)->GetInitialMomentum() << G4endl;
-    G4cout << "ParentID: "   << event->GetTrajectoryContainer()->GetVector()->at(i)->GetParentID() << G4endl;
-    G4cout << "PDGEncoding:" << event->GetTrajectoryContainer()->GetVector()->at(i)->GetPDGEncoding() << G4endl;
-  }
-  */
-}
+void nSimSCL2EventAction::EndOfEventAction(const G4Event*)
+{}
 
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
